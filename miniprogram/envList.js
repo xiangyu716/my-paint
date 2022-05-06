33a64dde141e23c163ce44491c2b27eb67a8f120
zhangxy <zhangxy@jackyun.com>
2022-05-06 17:19:36 +0800

    1.0.0

--- miniprogram/envList.js
+++
@@ -1,6 +0,0 @@
-const envList = [{"envId":"cloud1-1g8k44k63d4b7a1d","alias":"cloud1"}]
-const isMac = false
-module.exports = {
-    envList,
-    isMac
-}
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,9 +6,10 @@ Page({
     activePosi: {},
     isShowConfig: false,
     activeTool: '',
-    brush: {
-      width: 3, // 画笔宽度
-      color: {
+    config: {
+      brushWidth: 3,
+      escapeWidth: 30,
+      brushColor: {
         r: 0,
         g: 0,
         b: 0,
@@ -22,7 +23,7 @@ Page({
       }
     },
     oldBgColorValue: '',
-    isEscaping: false
+    isEscaping: false,
   },
   onReady() {
     wx.createSelectorQuery()
@@ -40,6 +41,12 @@ Page({
         this.setData({ myCanvas, myCtx, canvasInfo });
         this.resetCanvas();
       })
+  },
+  onShareTimeline() {
+    return {};
+  },
+  onShareAppMessage() {
+    return {}
   },
   // 用户触摸屏幕开始时
   touchStart(e) {
@@ -70,108 +77,87 @@ Page({
     let { x, y } = this.data.activePosi;
     let ctx = this.data.myCtx;
     ctx.beginPath();
-    ctx.arc(x, y, this.data.brush.width / 2, 0, 2*Math.PI)
+    ctx.arc(x, y, this.data.config.brushWidth / 2, 0, 2*Math.PI)
     ctx.fill();
-  },
-  animateRoutes: {
-    configShow: [
-      { bottom: '-60vh', ease: 'ease' },
-      { bottom: '0', ease: 'ease' }
-    ],
-    configHide: [
-      { bottom: '0', ease: 'ease-out' },
-      { bottom: '-60vh', ease: 'ease-out' }
-    ],
-    canvasShow: [
-      { top: '-100vh', opacity: 0, ease: 'ease-out' },
-      { top: '0', opacity: 0, ease: 'ease-out' },
-      { top: '0', opacity: 1, ease: 'ease-out' },
-    ],
-    canvasHide: [
-      { top: '0', opacity: 1, ease: 'ease-out' },
-      { top: '0', opacity: 0, ease: 'ease-out' },
-      { top: '-100vh', opacity: 0, ease: 'ease-out' }
-    ]
   },
   // 改变设置弹窗显示与隐藏
   // 打开弹窗时，用图片代替canvas，解决canvas层级过高、弹窗不显示的问题；
   changeConfigShow() {
     this.setData({ isShowConfig: !this.data.isShowConfig })
     if (this.data.isShowConfig) { // 打开设置弹窗
-      this.setData({oldBgColorValue: this.data.brush.bgColor.value}); // 记录画板颜色值
+      this.setData({oldBgColorValue: this.data.config.bgColor.value}); // 记录画板颜色值
       this.canvasToImg(imgUrl => {
         this.setData({imgUrl})
-        // 抽屉显示-上升动画
-        this.animate('#config-popup', this.animateRoutes.configShow, 100);
-        // 让canvas不露痕迹地消失
-        this.animate('#my-canvas', this.animateRoutes.canvasHide, 100);
       })
     } else { // 关闭设置弹窗
       // 更换画板颜色需要重置画布
-      if (this.data.oldBgColorValue !== this.data.brush.bgColor.value) {
+      if (this.data.oldBgColorValue !== this.data.config.bgColor.value) {
         this.resetCanvas();
       }
-      // 抽屉隐藏-下降动画
-      this.animate('#config-popup', this.animateRoutes.configHide, 100)
-      // 让canvas不露痕迹地出现
-      this.animate('#my-canvas', this.animateRoutes.canvasShow, 100);
     }
   },
   // 当画笔宽度改变时
   onBrushWidthChange(e) {
-    let brush = this.data.brush;
-    brush.width = e.detail.value;
-    this.setData({ brush });
+    let config = this.data.config;
+    config.brushWidth = e.detail.value;
+    this.setData({ config });
+    this.resetBrush();
+  },
+  // 当画笔宽度改变时
+  onEscapeWidthChange(e) {
+    let config = this.data.config;
+    config.escapeWidth = e.detail.value;
+    this.setData({ config });
     this.resetBrush();
   },
   // 当画笔颜色拖动改变时
   onBrushColorChange(e) {
-    let brush = this.data.brush,
-        color = brush.color;
-    brush.color[e.target.dataset.name] = e.detail.value;
-    brush.color.value = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    this.setData({ brush });
+    let config = this.data.config,
+        color = config.brushColor;
+    config.brushColor[e.target.dataset.name] = e.detail.value;
+    config.brushColor.value = `rgb(${color.r}, ${color.g}, ${color.b})`;
+    this.setData({ config });
     this.resetBrush();
   },
   // 当画笔颜色输入改变时
   onBrushColorInput(e) {
-    let brush = this.data.brush,
-        color = brush.color,
+    let config = this.data.config,
+        color = config.brushColor,
         value = e.detail.value;
     if (value < 0) value = 0;
     if (value > 255) value = 255;
-    brush.color[e.target.dataset.name] = value;
-    brush.color.value = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    this.setData({ brush });
+    config.brushColor[e.target.dataset.name] = value;
+    config.brushColor.value = `rgb(${color.r}, ${color.g}, ${color.b})`;
+    this.setData({ config });
     this.resetBrush();
   },
   // 当画板颜色拖动改变时
   onBgColorChange(e) {
-    let brush = this.data.brush,
-        color = brush.bgColor,
+    let config = this.data.config,
+        color = config.bgColor,
         value = e.detail.value;
         // 控制数值边界
     value = value < 0 ? 0 : value > 255 ? 255 : value;
-    if (brush.bgColor[e.target.dataset.name] == value) return;
+    if (config.bgColor[e.target.dataset.name] == value) return;
     // 每次打开设置弹窗，第一次修改花板颜色的时候让用户二次确认
-    if (brush.bgColor.value === this.data.oldBgColorValue) {
+    if (config.bgColor.value === this.data.oldBgColorValue) {
       wx.showModal({
         title: '提示',
         content: '更改画板颜色会清空当前内容，确定继续？',
         success: (res) => {
           if (res.confirm) {
-            brush.bgColor[e.target.dataset.name] = value;
-            brush.bgColor.value = `rgb(${color.r}, ${color.g}, ${color.b})`;
-            this.setData({ brush });
+            config.bgColor[e.target.dataset.name] = value;
+            config.bgColor.value = `rgb(${color.r}, ${color.g}, ${color.b})`;
+            this.setData({ config });
           } else if (res.cancel) {
-            this.setData({ brush });
+            this.setData({ config });
           }
         }
       })
     } else {
-      brush.bgColor[e.target.dataset.name] = value;
-      brush.bgColor.value = `rgb(${color.r}, ${color.g}, ${color.b})`;
-      this.setData({ brush });
+      config.bgColor[e.target.dataset.name] = value;
+      config.bgColor.value = `rgb(${color.r}, ${color.g}, ${color.b})`;
+      this.setData({ config });
     }
   },
   // 保存图片到本地
@@ -193,39 +179,56 @@ Page({
     })
   },
   // 擦除
-  escape() {
-    this.setData({isEscaping: !this.data.isEscaping});
+  escape(isReset = false) {
+    // 不是重置配置，则为用户点击
+    isReset !== true && this.setData({isEscaping: !this.data.isEscaping});
     if (this.data.isEscaping) {
-      this.data.myCtx.strokeStyle = this.data.brush.bgColor.value;
-      this.data.myCtx.fillStyle = this.data.brush.bgColor.value;
+      this.data.myCtx.strokeStyle = this.data.config.bgColor.value;
+      this.data.myCtx.fillStyle = this.data.config.bgColor.value;
+      this.data.myCtx.lineWidth = this.data.config.escapeWidth;
     } else {
-      this.data.myCtx.strokeStyle = this.data.brush.color.value;
-      this.data.myCtx.fillStyle = this.data.brush.color.value;
+      this.data.myCtx.strokeStyle = this.data.config.brushColor.value;
+      this.data.myCtx.fillStyle = this.data.config.brushColor.value;
+      this.data.myCtx.lineWidth = this.data.config.brushWidth;
     }
+  },
+  // 清空
+  clearCanvas() {
+    wx.showModal({
+      title: '提示',
+      content: '确定清空当前内容？',
+      success: (res) => {
+        if (res.confirm) {
+          this.resetCanvas();
+        }
+      }
+    })
   },
   // 重置画布--会清空当前内容
   resetCanvas() {
-    let {myCanvas, myCtx, brush, canvasInfo } = this.data;
+    let {myCanvas, myCtx, config, canvasInfo } = this.data;
     // 重置宽高
     myCanvas.width = canvasInfo.width;
     myCanvas.height = canvasInfo.height;
     // 缩放
     myCtx.scale(canvasInfo.dpr, canvasInfo.dpr);
     // 画笔宽度
-    myCtx.lineWidth = brush.width;
+    myCtx.lineWidth = config.brushWidth;
     // 画笔颜色
-    myCtx.strokeStyle = brush.color.value;
+    myCtx.strokeStyle = config.brushColor.value;
     // 先填充背景色
-    myCtx.fillStyle = brush.bgColor.value;
+    myCtx.fillStyle = config.bgColor.value;
     myCtx.fillRect(0, 0, canvasInfo.width, canvasInfo.height);
     // 再设置为画笔颜色
-    myCtx.fillStyle = brush.color.value;
+    myCtx.fillStyle = config.brushColor.value;
+    this.escape(true);
   },
   // 重置画笔--不会清空当前内容
   resetBrush() {
-    let { brush, myCtx } = this.data;
-    myCtx.lineWidth = brush.width;
-    myCtx.fillStyle = myCtx.strokeStyle = brush.color.value;
+    let { config, myCtx } = this.data;
+    myCtx.lineWidth = config.brushWidth;
+    myCtx.fillStyle = myCtx.strokeStyle = config.brushColor.value;
+    this.escape(true);
   },
 
 });
